64284c8cbc61eeac81804157a759d1e444e90f46
Ili <sion9801@gmail.com>
2021-10-12 21:33:28 -0400

    Prepare to add server

---
+++ FamilyReunionDinner2/Source/FamilyReunionDinner2/FamilyReunionDinner2.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class FamilyReunionDinner2 : ModuleRules
+{
+	public FamilyReunionDinner2(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+	}
+}
