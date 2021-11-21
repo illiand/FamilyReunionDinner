9a613a0503acf8a533ef908d37296b81666c6d2d
Kalpan <kalpan345@gmail.com>
2021-11-21 12:13:10 -0500

    game setup

--- FamilyReunionDinner2/Source/FamilyReunionDinner2/MyGameStateBase.cpp
+++ FamilyReunionDinner2/Source/FamilyReunionDinner2/MyGameStateBase.cpp
@@ -5,10 +5,35 @@
 
 void AMyGameStateBase::initGame()
 {
-	// LOADING THE RECIPE CARDS ON PRESSING 'T'
-	//load recipe/ingredients cards' data and create them into the scene
-	//TArray<FRecipeCardStruct> recipeFileData = UAPIClass::makeRecipeCards();
-	//TSubclassOf<ARecipeCard> recipeCard = LoadClass<ARecipeCard>(nullptr, TEXT("Blueprint'/Game/FirstPersonCPP/Blueprints/MyRecipeCard.MyRecipeCard_C'"));
+	TArray<FRecipeCardStruct> recipeFileData = UAPIClass::makeRecipeCards();
+	TSubclassOf<ARecipeCard> recipeCard = LoadClass<ARecipeCard>(nullptr, TEXT("Blueprint'/Game/FirstPersonCPP/Blueprints/MyRecipeCard.MyRecipeCard_C'"));
+	TArray<FIngredientCardStruct> ingredientFileData = UAPIClass::makeIngredientCards();
+	TSubclassOf<AIngredientCard> ingredientCard = LoadClass<AIngredientCard>(nullptr, TEXT("Blueprint'/Game/FirstPersonCPP/Blueprints/MyIngredientCard.MyIngredientCard_C'"));
+	TArray<FCookingCardStruct> cookingFileData = UAPIClass::makeCookingCards();
+	TSubclassOf<ACookingCard> cookingCard = LoadClass<ACookingCard>(nullptr, TEXT("Blueprint'/Game/FirstPersonCPP/Blueprints/MyCookingCard.MyCookingCard_C'"));
+	for (int i = 0; i < 5; i += 1)
+	{
+		float x = 40 + i % 5 * 16.5;
+		float y = 30 + i / 5 * 27;
+
+		ACookingCard* card = GetWorld()->SpawnActor<ACookingCard>(cookingCard, FVector(x, y, 64.5), FRotator(0, 180, 90), FActorSpawnParameters());
+		card->data = cookingFileData[i];
+		cooking.Add(card);
+	}
+	for (int i = 0; i < 4; i += 1)
+	{
+		float x = 95 + i % 5 * 16.5;
+		float y = -28 + i / 5 * 27;
+
+		ARecipeCard* card = GetWorld()->SpawnActor<ARecipeCard>(recipeCard, FVector(x, y, 53.5), FRotator(0, 0, 0), FActorSpawnParameters());
+		card->data = recipeFileData[i];
+		recipes.Add(card);
+
+		y = y + 25;
+		AIngredientCard* card2 = GetWorld()->SpawnActor<AIngredientCard>(ingredientCard, FVector(x, y, 53.5), FRotator(0, 0, 0), FActorSpawnParameters());
+		card2->data = ingredientFileData[i];
+		ingredients.Add(card2);
+	}
 	//for (int i = 0; i < recipeFileData.Num(); i += 1)
 	//{
 	//	float x = 40 + i % 5 * 16.5;
@@ -18,30 +43,6 @@ void AMyGameStateBase::initGame()
 	//	card->data = recipeFileData[i];
 	//	ingredients.Add(card);
 	//}
-	// LOADING THE INGREDIENT CARDS ON PRESSING 'T'
-	//TArray<FIngredientCardStruct> ingredientFileData = UAPIClass::makeIngredientCards();
-	//TSubclassOf<AIngredientCard> ingredientCard = LoadClass<AIngredientCard>(nullptr, TEXT("Blueprint'/Game/FirstPersonCPP/Blueprints/MyIngredientCard.MyIngredientCard_C'"));
-	//for (int i = 0; i < ingredientFileData.Num(); i += 1)
-	//{
-	//	float x = 40 + i % 5 * 16.5;
-	//	float y = -28 + i / 5 * 27;
-
-	//	AIngredientCard* card = GetWorld()->SpawnActor<AIngredientCard>(ingredientCard, FVector(x, y, 53.5), FRotator(0, 0, 0), FActorSpawnParameters());
-	//	card->data = ingredientFileData[i];
-	//	ingredients.Add(card);
-	//}
-	// LOADING THE COOKING CARDS ON PRESSING 'T'
-	TArray<FCookingCardStruct> ingredientFileData = UAPIClass::makeCookingCards();
-	TSubclassOf<ACookingCard> ingredientCard = LoadClass<ACookingCard>(nullptr, TEXT("Blueprint'/Game/FirstPersonCPP/Blueprints/MyCookingCard.MyCookingCard_C'"));
-	for (int i = 0; i < ingredientFileData.Num(); i += 1)
-	{
-		float x = 40 + i % 5 * 16.5;
-		float y = -28 + i / 5 * 27;
-
-		ACookingCard* card = GetWorld()->SpawnActor<ACookingCard>(ingredientCard, FVector(x, y, 53.5), FRotator(0, 0, 0), FActorSpawnParameters());
-		card->data = ingredientFileData[i];
-		ingredients.Add(card);
-	}
 }
 
 void AMyGameStateBase::nextTurn() 
